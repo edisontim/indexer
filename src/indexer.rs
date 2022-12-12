398d5f8a9824d99873dac597ecb61647d291f0af
Gregory Edison <gregory.edison1993@gmail.com>
2022-12-12 22:05:52 +0100

    feat: init indexer ingredients added

--- src/indexer.rs
+++ src/indexer.rs
@@ -1,14 +1,12 @@
+use crate::types::{AddedIngredientEvent, RecipeFactoryEventData};
 use std::{str, str::FromStr, thread};
 use thiserror::Error;
-use types::RecipeFactoryEventData;
 use web3::{
     futures::{future, StreamExt},
     transports::WebSocket,
-    types::{Block, BlockNumber, Filter, FilterBuilder, H160},
+    types::{BlockNumber, Filter, FilterBuilder, H160},
     Web3,
 };
-
-use crate::types;
 
 #[derive(Debug, Error)]
 pub enum IndexerError {
@@ -26,11 +24,7 @@ pub async fn init_main_indexer(
     let db = lfb_back::MongoRep::init("mongodb://localhost:27017/".to_string(), "lfb").unwrap();
 
     let ongoing_recipes = db.get_recipes_ongoing().map_err(IndexerError::from)?;
-    let recipe_contracts: Vec<(&str, i64)> = ongoing_recipes
-        .iter()
-        .map(|x| (&*x.address, x.last_block))
-        .collect::<Vec<(&str, i64)>>();
-
+    // first part: index all the recipes
     let current_block = w.eth().block_number().await?.as_u64();
     if let Some(last_recipe) = ongoing_recipes
         .iter()
@@ -51,20 +45,57 @@ pub async fn init_main_indexer(
             // parse logs and add to db
             let mut events: Vec<RecipeFactoryEventData> = logs
                 .into_iter()
-                .map(|x| types::RecipeFactoryEventData::from_log(&x))
+                .map(|x| RecipeFactoryEventData::from_log(&x))
                 .collect();
             events.iter_mut().for_each(|event| {
                 db.add_recipe(
-                    &event.recipe_contract_address,
+                    &event.recipe_address,
                     event.ingredients.iter_mut().map(|x| x.as_str()).collect(),
                     event.block,
                 );
             });
         }
     }
-    // TODO update recipe contracts from logs -> hash map.
-    // TODO all recipe contracts (in one eth query or for each contract?), query logs for IngredientAdded.
-    // If RecipeCompleted, remove from recipe contracts.
+
+    // second part: index all the ingredients for those recipes
+    let ongoing_recipes = db.get_recipes_ongoing().map_err(IndexerError::from)?;
+    if let Some(last_recipe) = ongoing_recipes
+        .iter()
+        .max_by(|&x, &y| x.last_block.cmp(&y.last_block))
+    {
+        for x in ((last_recipe.last_block as u64)..current_block).step_by(501) {
+            // get logs for the ingredients
+            let recipes_addresses = ongoing_recipes
+                .iter()
+                .map(|x| H160::from_str(x.address.as_str()).unwrap())
+                .collect::<Vec<H160>>();
+            let logs = w
+                .eth()
+                .logs(
+                    FilterBuilder::default()
+                        .address(recipes_addresses)
+                        .from_block(BlockNumber::from(x))
+                        .to_block(BlockNumber::from(x + 500))
+                        .build(),
+                )
+                .await?;
+            // parse logs and add to db
+            let mut events: Vec<AddedIngredientEvent> = logs
+                .into_iter()
+                .map(|x| AddedIngredientEvent::from_log(&x))
+                .collect();
+            events.iter_mut().for_each(|event| {
+                db.update_recipe(
+                    &event.recipe_address,
+                    &event.hash,
+                    &event.owner,
+                    event.block,
+                );
+            });
+        }
+    }
+    // TODO filter out the recipes completed events based on the topic. Call update_recipe_completed if event == recipe completed.
+    // TODO change the last updated block by querying from the mongo API function get_last_block
     Ok(vec![])
 }
 
